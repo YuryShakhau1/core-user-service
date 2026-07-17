2d024b279df463072f847c725021b599e74abedb
Yury Shakhau <shakhauyury@gmail.com>
2026-07-17 12:24:46 +0300

    Created raw user service application

---
+++ src/main/java/by/shakhau/core/user/CoreUserServiceApplication.java
@@ -0,0 +1,13 @@
+package by.shakhau.core.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/shakhau/core/user/CoreUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.shakhau.core.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
